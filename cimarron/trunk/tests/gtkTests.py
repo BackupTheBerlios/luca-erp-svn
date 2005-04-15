f1c8e33c3f9dcd4d742bde9ed3ecf3ab5ab2e4dc
mdione_at_fvl <mdione_at_fvl@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-04-15 20:00:32 +0000

    M    gtkTests.py + tests for gtk parenting and focusIn/Out.

--- cimarron/trunk/tests/gtkTests.py
+++ cimarron/trunk/tests/gtkTests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from papo.cimarron import skin
+
 from commonTests import abstractTestWidget, abstractTestVisibility
 from windowTests import TestWindow
 from labelTests import TestLabel
@@ -16,6 +18,13 @@ __all__ = ('TestGtkEntry',
            'TestGtkBoxes',
            )
 
+class testGtkParenting (abstractTestWidget):
+    def testGtkParent (self):
+        self.assertEqual (self.widget._widget.parent, self.parent._widget)
+
+    def testGtkChildInChildren (self):
+        self.assert_ (self.widget._widget in self.parent._widget.get_children ())
+
 class testGtkVisibility(abstractTestVisibility):
     def testGtkShow(self):
         self.app.show()
@@ -30,23 +39,64 @@ class testGtkVisibility(abstractTestVisibility):
                      or not self.widget._widget.window.is_visible(),
                      "calling hide on the widget didn't actually hide it")
 
-class TestGtkEntry(TestEntry):
+class testGtkFocusable(TestCase):
+    def testVisual (self):
+        b1= skin.Button (
+            parent= self.parent,
+            label= 'this',
+            onAction= self.focus1,
+            )
+        self.other= skin.Entry (parent=self.parent)
+        b2= skin.Button (
+            parent= self.parent,
+            label= 'that',
+            onAction= self.focus2,
+            )
+        print `self.parent`, `self.widget.parent`, `self.other.parent`, repr (list (self.parent.children))
+        self.widget.onFocusIn= self.here
+
+#         self.app.show ()
+#         self.app.run ()
+
+    def focus1 (self, *i):
+        print 'focus1'
+        self.widget._widget.grab_focus ()
+
+    def focus2 (self, *i):
+        print 'focus1'
+        self.other._widget.grab_focus ()
+
+    def here (self, *i):
+        print 'here!'
+
+    def testOnFocusIn (self):
+        other= skin.Entry (parent=self.parent)
+        self.widget.onFocusIn= self.focusOk
+        other._widget.grab_focus ()
+        self.widget._widget.grab_focus ()
+        self.assert_ (self.passed)
+
+    def focusOk (self):
+        self.passed= True
+
+class TestGtkEntry(testGtkFocusable, testGtkParenting, TestEntry):
     def testSetValue (self):
         self.widget.value= 'this is a test'
         self.assertEqual (self.widget._widget.get_text(), self.widget.value)
-    
+
 class TestGtkWindow(testGtkVisibility, TestWindow):
     pass
 
-class TestGtkLabel(TestLabel):
+class TestGtkLabel(testGtkParenting, TestLabel):
     def testSetLabel(self):
         self.widget.text= 'This is a label'
         self.assertEqual (self.widget.text, self.widget._widget.get_text ())
 
-class TestGtkButton(TestButton):
+class TestGtkButton(testGtkParenting, TestButton):
     def testSetLabel (self):
         self.widget.label= "Don't click me"
         self.assertEqual(self.widget.label, self.widget._widget.get_label ())
 
-class TestGtkBoxes(TestBoxes):
+class TestGtkBoxes(testGtkParenting, TestBoxes):
     pass
+
