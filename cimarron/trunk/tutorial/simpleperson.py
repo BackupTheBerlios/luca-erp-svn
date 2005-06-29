da454a4c135b1f2218b5aa8cfb0c12cbae6f7a04
jlenton <jlenton@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-06-29 08:26:28 +0000

    M    tutorial/tutorial.tex
    Changed 'App' to 'Application'. Fixed a typo.
    
    M    tutorial/simpleperson.py
    M    tutorial/simplepersoneditor.py
    
    reworked into partial sanity

--- cimarron/trunk/tutorial/simpleperson.py
+++ cimarron/trunk/tutorial/simpleperson.py
@@ -1,19 +1,7 @@
-class Person(object):
+from model.base import Model
+
+class Person(Model):
     def __init__(self, name=None, surname=None):
         super(Person, self).__init__()
-        self.set_name(name)
-        self.set_surname(surname)
-
-    def set_name(self, name):
-        self._name = name
-    def get_name(self):
-        return self._name
-    name = property(get_name, set_name, None,
-                    """The person's given name""")
-
-    def set_surname(self, surname):
-        self._surname = surname
-    def get_surname(self):
-        return self._surname
-    surname = property(get_surname, set_surname, None,
-                       """The person's family name""")
+        self.name = name
+        self.surname = surname
--- cimarron/trunk/tutorial/simplepersoneditor.py
+++ cimarron/trunk/tutorial/simplepersoneditor.py
@@ -3,35 +3,37 @@ from simpleperson import Person
 cimarron.config()
 
 class PersonEditor(cimarron.skin.WindowController):
-    def __init__(self, **kw):
+    def __init__(self, target=None, **kw):
         super(PersonEditor, self).__init__(**kw)
         self.win.title = 'Main Window'
-        vbox = cimarron.skin.VBox(parent=self.win)
-        hbox = cimarron.skin.HBox(parent=vbox)
-        cimarron.skin.Label(parent=hbox, text='Name:')
+        outer_vbox = cimarron.skin.VBox(parent=self.win)
+        hbox = cimarron.skin.HBox(parent=outer_vbox)
+        vbox = cimarron.skin.VBox(parent=hbox)
+        cimarron.skin.Label(parent=vbox, text='Name:')
+        cimarron.skin.Label(parent=vbox, text='Surname:')
+        vbox = cimarron.skin.VBox(parent=hbox)
         self.nameWidget = \
-            cimarron.skin.Entry(parent=hbox, attribute="name")
-        hbox = cimarron.skin.HBox(parent=vbox)
-        cimarron.skin.Label(parent=hbox, text='Surname:')
+            cimarron.skin.Entry(parent=vbox, attribute="name")
         self.surnameWidget = \
-            cimarron.skin.Entry(parent=hbox, attribute="surname")
-        cimarron.skin.Button(parent=vbox, label='Check', 
+            cimarron.skin.Entry(parent=vbox, attribute="surname")
+        cimarron.skin.Button(parent=outer_vbox, label='Check', 
 	     onAction = self.checkValues)
-        self.setTarget(self.target)
+        if target is not None:
+            self.newTarget(target)
 
     def checkValues(self, sender):
        print "Mr/Ms %s, %s" % (self.target.surname, self.target.name)
 
-    def setTarget(self, aPerson):
-        self.target = aPerson
-        self.nameWidget.newTarget(aPerson)
-        self.surnameWidget.newTarget(aPerson)
+    def newTarget(self, *a, **kw):
+        super(PersonEditor, self).newTarget(*a, **kw)
+        self.nameWidget.newTarget(self.target)
+        self.surnameWidget.newTarget(self.target)
 
-    def getTarget(self):
-        return self.target
+    def refresh(self):
+        self.nameWidget.refresh()
+        self.surnameWidget.refresh()
 
 app = cimarron.skin.Application()
-w = PersonEditor(parent=app)
+w = PersonEditor(parent=app, target=Person(name="John", surname="Doe"))
 w.show()
-w.setTarget(Person(name="John", surname="Doe"))
 app.run()
