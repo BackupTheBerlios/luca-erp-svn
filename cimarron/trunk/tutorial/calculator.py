bfdbd0cfbc563ae1233f45c62dbb1dee16054b81
jlenton <jlenton@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-07-01 03:31:06 +0000

    bugs and stuff

--- cimarron/trunk/tutorial/calculator.py
+++ cimarron/trunk/tutorial/calculator.py
@@ -1,3 +1,5 @@
+import operator
+
 from fvl import cimarron
 cimarron.config()
 
@@ -8,14 +10,14 @@ class CalculatorController(cimarron.skin.WindowController):
         vbox = cimarron.skin.VBox(parent=self.win)
         self.display = cimarron.skin.Label(parent=vbox, text='0')
         hbox = cimarron.skin.HBox(parent=vbox)
-        for aRow in [[['7', self.numberButton], ['4', self.numberButton],
-                      ['1', self.numberButton], ['C', self.clear]],
-                     [['8', self.numberButton], ['5', self.numberButton],
-                      ['2', self.numberButton], ['0', self.numberButton]],
-                     [['9', self.numberButton], ['6', self.numberButton], 
-                      ['3', self.numberButton], ['.', self.numberButton]],
-                     [['+', self.operate], ['-', self.operate], 
-                      ['*', self.operate],  ['/', self.operate]]]:
+        for aRow in ((('7', self.numberButton), ('4', self.numberButton),
+                      ('1', self.numberButton), ('C', self.clear)),
+                     (('8', self.numberButton), ('5', self.numberButton),
+                      ('2', self.numberButton), ('0', self.numberButton)),
+                     (('9', self.numberButton), ('6', self.numberButton), 
+                      ('3', self.numberButton), ('.', self.numberButton)),
+                     (('+', self.operate), ('-', self.operate), 
+                      ('*', self.operate),  ('/', self.operate))):
             vbox = cimarron.skin.VBox(parent=hbox)
             for aLabel, anAction in aRow:
                 cimarron.skin.Button(parent=vbox, label=aLabel, onAction=anAction)
@@ -31,24 +33,23 @@ class CalculatorController(cimarron.skin.WindowController):
     def clear(self, sender=None):
         self.op = None
         self.display.text = '0'
+        self.X = 0
         self.resetInput = True
+
+    operations = { '+': operator.add,
+                   '-': operator.sub,
+                   '*': operator.mul,
+                   '/': operator.div }
 
     def operate(self, sender=None):
         if not self.resetInput:
-            if self.op == None:
-                self.X = float(self.display.text)
-            else:
-                if self.op == '+':
-                    self.X = self.X + float(self.display.text)
-                elif self.op == '-':
-                    self.X = self.X - float(self.display.text)
-                elif self.op == '*':
-                    self.X = self.X * float(self.display.text)
-                elif self.op == '/':
-                    self.X = self.X / float(self.display.text)
-                self.display.text = str(self.X)
-                self.resetInput = True
-        self.op = sender.label
+            val = float(self.display.text)
+            if self.op is not None:
+                val = self.op(self.X, val)
+                self.display.text = str(val)
+            self.resetInput = True
+            self.X = val
+        self.op = self.operations[sender.label]
 
 app = cimarron.skin.Application()
 w = CalculatorController(parent=app)
