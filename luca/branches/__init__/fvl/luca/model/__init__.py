747cfc8231f6ec7db7b76dc79322c4616d4ed14a
jlenton <jlenton@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-07-14 22:40:43 +0000

    tests fail because there is no way they will pass until we get a common in which to put fvl/__init__.py :-/

---
+++ luca/branches/__init__/fvl/luca/model/__init__.py
@@ -0,0 +1,29 @@
+from Modeling import ModelSet, Model, dynamic
+
+model = Model.loadModel('luca.py')
+ModelSet.defaultModelSet().addModel(model)
+
+class LucaMeta(dynamic.CustomObjectMeta):
+    def __new__(cls, className, bases, namespace):
+        namespace['mdl_define_properties'] = 1
+        return super(LucaMeta, cls).__new__(cls, className, bases, namespace)
+
+__metaclass__ = LucaMeta
+
+# now the idea is that, for every class in the model you create a
+# class, and add methods to it if necessary. E.g,
+#   class Person:
+#       pass
+# since this would be tedious and error-prone, just define the classes
+# you actually need below, and we'll automatically generate the rest
+# (below).
+# be sure to ad the classes *below* this line
+
+
+
+# just be sure to add the classes *above* this line
+namespace = globals()
+print model.entitiesNames()
+for className in model.entitiesNames():
+    if className not in namespace:
+        namespace[className] = LucaMeta(className, (), {})
