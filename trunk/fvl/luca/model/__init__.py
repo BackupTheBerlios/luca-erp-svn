6d1887750bdc87839f339b804a36389d73053671
jlenton <jlenton@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-07-15 13:48:55 +0000

    formalities, formalities, formalities

--- trunk/fvl/luca/model/__init__.py
+++ trunk/fvl/luca/model/__init__.py
@@ -1,8 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005 Fundación Via Libre
+#
+# This file is part of PAPO.
+#
+# PAPO is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# PAPO is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PAPO; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307 USA
+
+__revision__ = int('$Rev: 200 $'[5:-1])
+
 import os
+import logging
 
 from Modeling import ModelSet, Model, dynamic
 
-model = Model.searchModel('luca', os.path.dirname(__file__))
+logger = logging.getLogger('fvl.luca.model')
+
+model_name = os.path.join(os.path.dirname(__file__), 'pymodel_luca.py')
+try:
+    model = Model.loadModel(model_name)
+except ImportError:
+    logger.critical("I couldn't load the pymodel from file %r. Please read INSTALL.txt" % model_name)
+    raise
+del model_name
 ModelSet.defaultModelSet().addModel(model)
 
 class LucaMeta(dynamic.CustomObjectMeta):
@@ -25,7 +56,6 @@ __metaclass__ = LucaMeta
 
 # just be sure to add the classes *above* this line
 namespace = globals()
-print model.entitiesNames()
 for className in model.entitiesNames():
     if className not in namespace:
         namespace[className] = LucaMeta(className, (), {})
