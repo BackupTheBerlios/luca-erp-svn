52ef7546d6baafc15c15e350bcfc1edc43e78dec
jlenton <jlenton@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-07-21 21:47:05 +0000

    this does *not* work

--- luca/trunk/stockAdjustment.py
+++ luca/trunk/stockAdjustment.py
@@ -20,48 +20,52 @@
 
 __revision__ = int('$Rev: 200 $'[5:-1])
 
-from fvl import cimarron
+from fvl.cimarron.skin import Column, HBox, VBox, Button, Checkbox, Grid, \
+     Search, WindowController, Application
 from fvl.luca.model import Product, Stock
 from fvl.luca.transaction import Transaction
 
-class StockAdjustmentWindow(cimarron.skin.WindowController):
+class StockAdjustmentWindow(WindowController):
     def __init__(self, **kw):
         super(StockAdjustmentWindow, self).__init__(**kw)
         self.win.title = "Stock Adjustment"
         self.trans = Transaction()
         
-        v = cimarron.skin.VBox(parent=self.win)
+        v= VBox (parent=self.win)
 
-        columnas = (cimarron.skin.Column(name="Code", attribute="product.code", readOnly= True),
-                    cimarron.skin.Column(name="Name", attribute="product.name", readOnly= True))
+        columns = (Column(name="Code", attribute="product.code", readOnly=True),
+                   Column(name="Name", attribute="product.name", readOnly=True))
 
-        self.searcher = cimarron.skin.Search(parent=v, columns=columnas,
-                                             cls=Stock, searcher=self.trans,
-                                             onAction=self.listValues)
+        self.searcher = Search(parent=v, columns=columns,
+                               cls=Stock, searcher=self.trans,
+                               onAction=self.listValues)
 
-        columnas = (cimarron.skin.Column(name="Code", attribute="product.code", readOnly= True),
-                    cimarron.skin.Column(name="Name", attribute="product.name", readOnly= False),
-                    cimarron.skin.Column(name="Level", attribute="level"))
-        self.stockEditor = cimarron.skin.Grid(parent=v, columns=columnas)
-        actionContainer = cimarron.skin.HBox(parent=v)
-        save = cimarron.skin.Button(parent=actionContainer, label="Save", onAction=self.save)
-        discard = cimarron.skin.Button(parent=actionContainer, label="Discard", onAction=self.discard)
+        columns = (Column(name="Code", attribute="product.code", readOnly=True),
+                   Column(name="Name", attribute="product.name", readOnly=True),
+                   Column(name="Level", attribute="level"))
+        self.stockEditor = Grid(parent=v, columns=columns)
+        actionContainer = HBox(parent=v, expand=False)
+        save = Button(parent=actionContainer, label="Save", onAction=self.save)
+        discard = Button(parent=actionContainer, label="Discard", onAction=self.discard)
 
-    def listValues(self,sender):
-        self.stockEditor.commitValue(sender.value)
+    def commitValue(self, value):
+        self.stockEditor.commitValue(value)
+        print 'fix grid, get rid of refresh here'
         self.stockEditor.refresh()
+
+    def listValues(self, sender):
+        self.commitValue(sender.value)
 
     def save(self, *ignore):
         self.trans.save()
 
     def discard(self, *ignore):
         self.trans.discard()
-        self.stockEditor.commitValue(None)
-        self.stockEditor.refresh()
+        self.commitValue(None)
 
 
 if __name__=='__main__':
-    a = cimarron.skin.Application()
+    a = Application()
     w = StockAdjustmentWindow(parent=a)
     w.show()
     a.run()
