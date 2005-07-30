b71a4ce929646e5b108d41f0d49e27f778003048
fheinz <fheinz@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-07-30 22:13:47 +0000

    Refined the tutorial, added a brief explanation of the delegation interface

--- cimarron/trunk/tutorial/simplepersoneditor.py
+++ cimarron/trunk/tutorial/simplepersoneditor.py
@@ -13,9 +13,9 @@ class PersonEditor(cimarron.skin.WindowController):
         vbox = cimarron.skin.VBox(parent=hbox)
         self.widgets = \
             (cimarron.skin.Entry(parent=vbox, attribute="name",
-                                 onAction=self.checkValues),
+                                 onAction=self.checkValues, delegates=[self]),
              cimarron.skin.Entry(parent=vbox, attribute="surname",
-                                 onAction=self.checkValues))
+                                 onAction=self.checkValues, delegates=[self]))
         cimarron.skin.Button(parent=outer_vbox, label='Check', 
 	     onAction = self.checkValues)
         self.label = cimarron.skin.Label(parent=outer_vbox, text="",
@@ -26,6 +26,13 @@ class PersonEditor(cimarron.skin.WindowController):
     def checkValues(self, sender=None):
         self.label.text = "Mr/Ms %s, %s" % \
                           (self.target.surname, self.target.name)
+
+    def will_focus_out(self, control):
+        if control.value:
+            control.value = control.value.strip().title()
+        if not control.value:
+            return cimarron.skin.ForcedNo
+        return cimarron.skin.Yes
 
     def newTarget(self, target):
         super(PersonEditor, self).newTarget(target)
