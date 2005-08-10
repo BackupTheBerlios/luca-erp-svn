4d1f61c8af8559b7e63498051978c38b6b6ad4dc
mdione_at_fvl <mdione_at_fvl@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-08-10 17:53:09 +0000

    M    fvl/luca/model/__init__.py * fixed bug that unicodified everything. now we just unicodify strings   and such.
    M    test/testReceipt.py
    * fixed tests that exploited that bug.

--- trunk/test/testReceipt.py
+++ trunk/test/testReceipt.py
@@ -13,13 +13,13 @@ class TestReceipt(unittest.TestCase):
 
     def testRefresh(self):
         for entry, value in (("person", Person(name="Jose", surname="Perez")),
-                             ("amount", '123.98'), ("concept", "whatever"),
+                             ("amount", 123.98), ("concept", "whatever"),
                              ("date", "12/03/2005")):
             getattr(self.widget, entry).commitValue(value)
             if entry == "date":
                 """
                 Ugly fix so the Date in entry can be compared with the one commited
                 """
-                self.assertEqual(self.widget.value.getattr(entry), str(DateTimeFrom(str(value))))
+                self.assertEqual(self.widget.value.getattr(entry), DateTimeFrom(value))
             else:
                 self.assertEqual(self.widget.value.getattr(entry), value)
