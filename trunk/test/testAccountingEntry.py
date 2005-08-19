bcbba786db37a674220fbeed8474916a3e829861
francis <francis@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-08-19 18:13:10 +0000

    Mostly added Revision tags and Copyright notice MM   test/testAccountingEntry.py M    test/testPointOfSale.py M    test/testReceipt.py M    fvl/luca/model/accounting_entry.py

--- trunk/test/testAccountingEntry.py
+++ trunk/test/testAccountingEntry.py
@@ -1,13 +1,31 @@
-__revision__ = int()
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005 Fundación Via Libre
+#
+# This file is part of PAPO.
+#
+# PAPO is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# PAPO is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PAPO; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA 02111-1307 USA
+
+__revision__ = int('$Rev$'[5:-1])
+
 
 from testWithDatabase import testWithDatabase
 from fvl.luca.model import MovementAccount, Movement, Currency
 from fvl.luca.model.accounting_entry import AccountingEntry
 from fvl.luca.model.money import Money
 from fvl.luca.model.point_of_sale  import PointOfSale
-#from
-#import  AccountingEntry, MovementAccount, Movement,\
-#                             Money, PointOfSale
 
 from fvl.luca.transaction import Transaction, Qualifier
 
