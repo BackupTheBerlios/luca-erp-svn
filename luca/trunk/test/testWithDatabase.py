94a7ae7ebc91008a367da99b9f267dfbcffb0084
mdione <mdione@8067ad51-12f4-0310-9d40-e2a641af10b2>
2005-08-22 20:51:32 +0000

    A    trunk/tools/ppython A    trunk/tools/coverage.py A    trunk/cimarron/test A    trunk/cimarron/setup.py A    trunk/cimarron/MANIFEST.in A    trunk/cimarron/Makefile D    luca/trunk/test D    luca/trunk/xins D    luca/trunk/basic_movement_accounts.sql D    luca/trunk/luca.xot D    luca/trunk/Makefile D    cimarron/trunk/test D    cimarron/trunk/tools/ppython D    cimarron/trunk/tools/coverage.py D    cimarron/trunk/setup.py D    cimarron/trunk/MANIFEST.in D    cimarron/trunk/Makefile * moving cimarron arround.

--- luca/trunk/test/testWithDatabase.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import os
-
-class testWithDatabase(unittest.TestCase):
-    _cache_db = 'luca_cache.db'
-    _test_db = 'luca_test.db'
-    def setUp(self):
-        os.system('cp -f %s %s' % (self._cache_db, self._test_db))
-        super(testWithDatabase, self).setUp()
